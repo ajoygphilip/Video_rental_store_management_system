84342e8bd3d88d6b65a9a818ba60547e5eac75e8
ajoygp <ajoygp551@gmail.com>
2023-08-08 09:58:10 +0530

    create movies model

--- moviesapp/models.py
+++ moviesapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    '''This model represents one movie and stores related details.'''
+    GENRE_CHOICES = (
+    ('action','Action'),
+    ('drama','Drama'),
+    ('comedy','Comedy'),
+    ('horror','Horror'),
+    ('thriller','Thriller'),
+    ('romance','Romance'),
+)
+    title = models.CharField(max_length=200)
+    description = models.TextField() 
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
