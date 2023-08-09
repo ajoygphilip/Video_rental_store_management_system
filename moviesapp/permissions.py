bbbff4dafa41e270206329a09cdbfad1b1124796
ajoygp <ajoygp551@gmail.com>
2023-08-09 10:27:14 +0530

    Add permissions

---
+++ moviesapp/permissions.py
@@ -0,0 +1,38 @@
+from rest_framework import permissions
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        if view.action in ['list', 'retrieve']:
+            return True
+
+        if view.action in ['create', 'update', 'partial_update', 'destroy']:
+            return request.user.is_superuser or request.user.profile.is_staff
+
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        # Deny actions on objects if the user is not authenticated
+        if not request.user.is_authenticated:
+            return False
+
+        if view.action == 'retrieve':
+            return True
+
+        else:
+            return request.user.is_superuser or request.user.profile.is_staff
+
+
+class IsStaff(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        else:
+            return request.user.profile.is_staff
