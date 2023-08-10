f02016b9c3999a6ab52e55cf5f8c5bf92ae54183
ajoygp <ajoygp551@gmail.com>
2023-08-10 13:00:50 +0530

    update urls basenames

--- moviesapp/urls.py
+++ moviesapp/urls.py
@@ -5,11 +5,11 @@ from .views import MovieViewset, MovieCopyViewset, RentedMovieViewset, RentedMov
 
 router = routers.SimpleRouter()
 
-router.register("inventory", MovieCopyViewset, basename="moviecopies")
+router.register("inventory", MovieCopyViewset, basename="movie-inventory")
 router.register("rented/history", RentedMovieHistoryViewset,
-                basename="rentalrecord")
-router.register("rented", RentedMovieViewset, basename="rentalrecord")
-router.register("catalouge", MovieViewset, basename="movies")
+                basename="rental-record-history")
+router.register("rented", RentedMovieViewset, basename="current-rental-record")
+router.register("catalouge", MovieViewset, basename="movies-catalouge")
 
 urlpatterns = router.urls
 
