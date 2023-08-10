1e6cebcbc14c0afc5d2e5be0f9d305d331ad85e9
ajoygp <ajoygp551@gmail.com>
2023-08-10 12:54:23 +0530

    update urls

--- moviesapp/urls.py
+++ moviesapp/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from .views import MovieViewset, MovieCopyViewset, RentedMovieViewset,RentedMovieHistoryViewset
+from .views import MovieViewset, MovieCopyViewset, RentedMovieViewset, RentedMovieHistoryViewset
 
 router = routers.SimpleRouter()
 
-router.register("copies", MovieCopyViewset, basename="moviecopies")
-router.register("rented/history", RentedMovieHistoryViewset, basename="rentalrecord")
+router.register("inventory", MovieCopyViewset, basename="moviecopies")
+router.register("rented/history", RentedMovieHistoryViewset,
+                basename="rentalrecord")
 router.register("rented", RentedMovieViewset, basename="rentalrecord")
-router.register("", MovieViewset, basename="movies")
+router.register("catalouge", MovieViewset, basename="movies")
 
 urlpatterns = router.urls
